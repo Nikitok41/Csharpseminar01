2177e4c71904e929562e958fb7e10647b302203c
Nikitok41 <nik777pups@gmail.com>
2022-05-10 11:13:37 +0300

    Добавил две задачи со звездочкой

---
+++ Task06dop/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу вычисления модуля числа.
+2 -> 2
+-3 -> 3
+-7 -> 7
+*/
+
+Console.WriteLine("Введите число для нахождения его модуля: ");
+int A = Convert.ToInt32(Console.ReadLine());
+if (A > 0){
+    Console.WriteLine(A);
+}
+else{
+    Console.WriteLine(-A);
+}
---
+++ Task07dop/Program.cs
@@ -0,0 +1,7 @@
+﻿/**. Напишите программу, которая на вход принимает значение, а
+на выходе показывает обратное значение.*/
+
+Console.WriteLine("Введите число для нахождения обратного значения числа: ");
+double A = System.Double.Parse(Console.ReadLine());
+double B = 1/A;
+Console.WriteLine(B);
